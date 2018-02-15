3903e5c1ed1a23292bce46c8cdba556d86ba1ced
iakshaygoyal <goyalakshay21@gmail.com>
2018-02-15 22:01:12 +0530

    Update BiggestQ.py

--- BiggestQ.py
+++ BiggestQ.py
@@ -1,7 +1,10 @@
 def biggest(dict):
-    values=dict.values()
-    ans=0
-    for v in values:
-        ans=max(ans, len(v))
+    items=dict.items()
+    max=-1
+    ans=None
+    for i in items:
+        if len(i[1]) > max:
+            max=len(i[1])
+            ans=i[0]
 
     return ans
